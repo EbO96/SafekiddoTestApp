6170276ed51522b56be4bfe96b10214c1bb72f7c
EbO96 <sebastian96rok@gmail.com>
2020-09-12 09:42:51 +0200

    Hot fix

--- app/src/main/java/com/safekiddo/testapp/data/rest/interceptor/NewsCacheInterceptor.kt
+++ app/src/main/java/com/safekiddo/testapp/data/rest/interceptor/NewsCacheInterceptor.kt
@@ -12,7 +12,6 @@ class NewsCacheInterceptor : Interceptor {
         val response = chain.proceed(chain.request())
 
         val cacheControl = CacheControl.Builder()
-                .noCache()
                 .maxAge(NewsRestService.MAX_CACHE_AGE_MS, TimeUnit.MILLISECONDS)
                 .build()
 
